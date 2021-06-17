47b3e05b6afc502c52baea8843056828cd780594
Lycheejam <lycheejamwow@gmail.com>
2021-06-17 23:03:49 +0900

    :+1: added footer

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,20 @@
     <v-main>
       <Jasper/>
     </v-main>
+    <Footer/>
   </v-app>
 </template>
 
 <script>
 import Jasper from './components/Jasper';
+import Footer from './components/Footer';
 
 export default {
   name: 'App',
 
   components: {
     Jasper,
+    Footer,
   },
 
   data: () => ({
---
+++ src/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-footer padless absolute fixed color="transparent">
+    <v-col
+      class="text-center white--text"
+      cols="12"
+      style="font-size: 12px;"
+    >
+      Copyright © {{ new Date().getFullYear() }} Ago Co.,Ltd. All Rights Reserved
+    </v-col>
+  </v-footer>
+</template>
